5b2b0563d74352cfab56deb7024aa1d838902ac4
Ramon Castillo <luisydiego201516@gmail.com>
2023-10-13 18:38:29 -0700

    agregada funcion para build

---
+++ build/img/imgPaginasD/paginaKanye/datos.php
@@ -0,0 +1,11 @@
+$nombrePagina = "":
+$descripcionPagina = "";
+
+$tecnologiaUsada = array(
+    "git" => true,
+    "gtml" => true,
+    "css" => true,
+    "sass" => true,
+    "js" => true,
+    "php" => true
+);
