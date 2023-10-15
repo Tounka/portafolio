ca043f179c2218dd0f136b5d808446333158d24c
Ramon Castillo <luisydiego201516@gmail.com>
2023-10-15 12:55:13 -0700

    agregada pagina portafolio al portafolio

--- src/js/arrayImg.js
+++ src/js/arrayImg.js
@@ -13,12 +13,13 @@ function Pagina(titulo, descripcion, tecnologias, url,imgGrande, imgPeque) {
   
   const paginaPomo = new Pagina("Pomo-Pomodoro", "Es el clásico reloj Pomodoro para mejorar la productividad, pero esta vez con música de tus artistas favoritos :D (solo 3, que tampoco soy una API...).  ", ["html", "css", "sass", "js", "git"], "https://pomo-pomodoro.netlify.app/", "build/img/imgPaginasD/paginaKanye/grande", "build/img/imgPaginasD/paginaKanye/peque");
   const paginaGravity = new Pagina("Gravity Falls Fan", "Es una versión hecha por mí mismo de la página oficial de Disney de la serie.", ["html", "css", "sass", "js", "git"], "https://gravity-falls-fanpage-p3.netlify.app/", "build/img/imgPaginasD/paginaGravityFalls/grande", "build/img/imgPaginasD/paginaGravityFalls/peque");
+  const paginaPortafolio = new Pagina("Portafolio", "Se trata de mi portafolio personal. Espero que te guste. :D Existen dos versiones, una hecha con PHP y otra con JavaScript (la que ves aquí es la hecha con JS). Netlify no soporta PHP, por lo que tuve que desarrollar la segunda.  Puedes ver las distintas versiones aquí. :D  ", ["html", "css", "sass", "js", "git", "php"], "https://github.com/Tounka/portafolio", "build/img/imgPaginasD/paginaPortafolio/grande", "build/img/imgPaginasD/paginaPortafolio/peque");
   const paginaPaseos = new Pagina("Paseos Castillo", "Se trata de una página web para la promoción de paseos hipotéticos. Fue mi primer proyecto, en esta página utilicé muchos elementos visuales generados por inteligencia artificial.", ["html", "css", "js"], "https://buscatupaseo.netlify.app/", "build/img/imgPaginasD/paginaPaseos/grande", "build/img/imgPaginasD/paginaPaseos/peque");
 
 
 
 
-const arregloDePaginas = [paginaPomo, paginaGravity, paginaPaseos];
+const arregloDePaginas = [paginaPomo, paginaGravity,paginaPortafolio, paginaPaseos];
 arregloDePaginas.forEach(function (pagina) {
   const contenedorSuperior = document.createElement('div');
   contenedorSuperior.classList.add('contenedorSuperiorPortafolio');
