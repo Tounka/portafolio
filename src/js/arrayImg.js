1893522bb26c6886a3adfb71d8a0cd88c18f00ae
Tounka <luisydiego201516@gmail.com>
2023-11-07 09:59:45 -0800

    Se ha agregado la página cocina y se han realizado mejoras en las imágenes.

--- src/js/arrayImg.js
+++ src/js/arrayImg.js
@@ -16,10 +16,12 @@ function Pagina(titulo, descripcion, tecnologias, url,imgGrande, imgPeque) {
   const paginaPortafolio = new Pagina("Portafolio", "Se trata de mi portafolio personal. Espero que te guste. :D Existen dos versiones, una hecha con PHP y otra con JavaScript (la que ves aquí es la hecha con JS). Netlify no soporta PHP, por lo que tuve que desarrollar la segunda.  Puedes ver las distintas versiones aquí. :D  ", ["html", "css", "sass", "js", "git", "php"], "https://github.com/Tounka/portafolio", "build/img/imgPaginasD/paginaPortafolio/grande", "build/img/imgPaginasD/paginaPortafolio/peque");
   const paginaPaseos = new Pagina("Paseos Castillo", "Se trata de una página web para la promoción de paseos hipotéticos. Fue mi primer proyecto, en esta página utilicé muchos elementos visuales generados por inteligencia artificial.", ["html", "css", "js"], "https://buscatupaseo.netlify.app/", "build/img/imgPaginasD/paginaPaseos/grande", "build/img/imgPaginasD/paginaPaseos/peque");
   const paginaSolar = new Pagina("SolarWeb", "Se trata de una pagina web enfocada a la verificar la factibilidad de la compra de paneles solares, en esta pagina aprendí el uso de bootstrap.", ["html", "css", "sass", "js", "git", "bootstrap"], "https://solarong.netlify.app/", "build/img/imgPaginasD/paginaSolar/grande", "build/img/imgPaginasD/paginaSolar/peque");
+  const paginaCocina = new Pagina("Solución Especifica Cocina con IA", "Esta pagina se trata de una solución especifica para mi cocina, es una pagina que utilizare de forma personal en mi cocina. En esta podre ver diferentes imágenes personales cada vez que recargue la pagina, ver el clima, reproducir videos, crear recetas mediante inteligencia artificial (API GPT), etc... El link que se encuentra en las imágenes es una version capada de la pagina (Sin la funcionalidad IA, por el costo asociado a la función)", ["html", "css", "sass", "js", "git", "bootstrap"], "https://6547d8f4b9207a76c640ad49--cocinacasatounka.netlify.app/", "build/img/imgPaginasD/paginaCocina/grande", "build/img/imgPaginasD/paginaCocina/peque");
+  //const pagina = new Pagina("", "", ["html", "css", "sass", "js", "git", "bootstrap"], "linkPagina", "build/img/imgPaginasD/carpeta/grande", "build/img/imgPaginasD/carpeta/peque");
 
 
 
-const arregloDePaginas = [paginaSolar, paginaPomo, paginaGravity,paginaPortafolio, paginaPaseos];
+const arregloDePaginas = [paginaCocina,paginaSolar, paginaPomo, paginaGravity,paginaPortafolio, paginaPaseos];
 arregloDePaginas.forEach(function (pagina) {
   const contenedorSuperior = document.createElement('div');
   contenedorSuperior.classList.add('contenedorSuperiorPortafolio');
